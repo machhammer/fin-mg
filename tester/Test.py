e77530b32f2b954be902f3f0e78e822c505050dc
Markus Achhammer <machhammer@markuss-macbook-pro.home>
2019-07-05 20:53:28 +0200

    selenium

--- tester/Test.py
+++ tester/Test.py
@@ -7,5 +7,6 @@ time.sleep(5) # Let the user actually see something!
 search_box = driver.find_element_by_name('q')
 search_box.send_keys('ChromeDriver')
 search_box.submit()
+
 time.sleep(5) # Let the user actually see something!
 driver.quit()
