1ed5dc1c30024ab7bdaf3ed3346ab73e82ca13e7
Markus Achhammer <machhammer@markuss-macbook-pro.home>
2019-07-06 17:46:16 +0200

    new setup

---
+++ install_ssh_node.sh
@@ -0,0 +1,48 @@
+#Slave
+sudo su
+adduser jenkins-slave-01
+sudo su - jenkins-slave-01
+ssh-keygen -t rsa -N "" -f /home/jenkins-slave-01/.ssh/id_rsa
+cd .ssh
+cat id_rsa.pub > authorized_keys
+chmod 700 authorized_keys
+
+cd..
+mkdir workspace
+
+exit
+exit
+
+sudo su ubuntu
+sudo apt-get upgrade
+sudo apt-get update
+sudo apt-get install default-jdk
+
+#DOCKER
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable"
+sudo apt-get update
+sudo apt-get install docker-ce
+apt-cache madison docker-ce
+
+sudo usermod -aG docker jenkins-slave-01
+
+
+
+#Master
+
+sudo mkdir -p /var/lib/jenkins/.ssh
+cd /var/lib/jenkins/.ssh
+sudo su 
+ssh-keyscan -H 172.31.42.254 >> /var/lib/jenkins/.ssh/known_hosts
+chown ubuntu:ubuntu known_hosts
+chmod 777 known_hosts
+
+
+
+#Stock Docker
+
+docker stop $(docker ps -aq)
+docker rmi $(docker images -q)
